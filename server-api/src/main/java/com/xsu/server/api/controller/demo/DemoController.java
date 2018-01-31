5d13b1e232fcb7a018007834fc6cd156d0252930
suxiang <xsu@leqee.com>
2018-01-31 14:56:02 +0800

    可以正常运行了，加了logback日志功能

--- server-api/src/main/java/com/xsu/server/api/controller/demo/DemoController.java
+++ server-api/src/main/java/com/xsu/server/api/controller/demo/DemoController.java
@@ -2,20 +2,25 @@ package com.xsu.server.api.controller.demo;
 
 import com.xsu.server.biz.DemoBiz;
 import com.xsu.server.sdk.demo.DemoDomain;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
+    private Logger logger = LoggerFactory.getLogger(DemoController.class);
 
     @Autowired
     private DemoBiz demoBiz;
 
     @RequestMapping("/demo")
     public String demo(){
+        logger.info("this is demoController‘s logger");
         DemoDomain domain=demoBiz.demoShow("controller");
         System.out.println(domain);
+
 
         DemoDomain domain2=new DemoDomain();
         domain2.setDemoId(1L);
